793a4e01641aaa7f168ca3e46ff38ce5b5092f3b
Aleksey <aleks-sh@mail.ru>
2020-12-13 23:28:01 +0300

    genre author

---
+++ !reset_author.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE author (
+      author_id INT PRIMARY KEY AUTO_INCREMENT, 
+      name_author VARCHAR(50) 
+      )ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+insert into author (name_author) value
+('Булгаков М.А.'),
+('Достоевский Ф.М.'),
+('Есенин С.А.'),
+('Пастернак Б.Л.'),
+('Лермонтов М.Ю.');
---
+++ !reset_genre.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `genre`;
+CREATE TABLE genre (
+      genre_id INT PRIMARY KEY AUTO_INCREMENT, 
+      name_genre VARCHAR(50) 
+      )ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+insert into genre (name_genre) value
+('Роман'),
+('Поэзия'),
+('Приключения');
