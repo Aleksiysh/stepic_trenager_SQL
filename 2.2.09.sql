ad913a9acf73aa7b0389ee5dd94fb957e2145ce8
Aleksey <aleks-sh@mail.ru>
2020-12-13 23:48:27 +0300

    2.2.10

---
+++ 2.2.09.sql
@@ -0,0 +1,20 @@
+/*
+Текст задания (чтобы не прокручивать страницу):
+Если в таблицах supply  и book есть одинаковые книги, 
+которые имеют равную цену,  вывести их название и автора, 
+а также посчитать общее количество экземпляров книг в 
+таблицах supply и book,  столбцы назвать 
+Название, Автор  и Количество.
+*/
+
+SELECT 
+    book.title AS Название,
+    author.name_author AS Автор,
+    supply.amount + book.amount AS Количество
+FROM
+    book
+        JOIN
+    author USING (author_id)
+        JOIN
+    supply ON supply.title = book.title
+        AND supply.price = book.price;
