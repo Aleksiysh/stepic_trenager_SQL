71a5d5a0e2dad50ee4a9064f87e59bec6c0e2448
Aleksey <aleks-sh@mail.ru>
2020-12-14 05:53:43 +0300

    modified:   internet_store/!!!.sql

--- internet_store/!!!.sql
+++ internet_store/!!!.sql
@@ -65,16 +65,99 @@ insert into city (name_city,days_delivery) value
 ('Владивосток',12);
 
 CREATE TABLE client (
-      client_id INT PRIMARY KEY AUTO_INCREMENT,
-      name_client VARCHAR(50) ,
-      city_id int,
-      email varchar(30),
-      FOREIGN KEY (city_id)
+    client_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_client VARCHAR(50),
+    city_id INT,
+    email VARCHAR(30),
+    FOREIGN KEY (city_id)
         REFERENCES city (city_id)
         ON DELETE SET NULL
-      )ENGINE='InnoDB' AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
+      
 insert into client (name_client,city_id,email) value
 ("Баранов Павел",	3,	"baranov@test"),
 ("Абрамова Катя",	1,	"abramova@test"),
 ("Семенов Иван",	2,	"semenov@test"),
 ("Яковлева Галина",	1,	"yakovleva@test");
+
+CREATE TABLE buy  (
+      buy_id INT PRIMARY KEY AUTO_INCREMENT,
+      buy_description	 VARCHAR(100) ,
+      client_id int,
+      FOREIGN KEY (client_id)
+        REFERENCES client (client_id)
+        ON DELETE SET NULL
+      )ENGINE='InnoDB' AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+      
+insert into buy  (buy_description,client_id) value
+("Доставка только вечером",	1),
+(null,	 	3),
+("Упаковать каждую книгу по отдельности",	2),
+(null,	 	1);
+
+CREATE TABLE buy_book (
+    buy_book_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id INT,
+    book_id INT,
+    amount int,
+    FOREIGN KEY (buy_id)
+        REFERENCES buy (buy_id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (book_id)
+        REFERENCES book (book_id)
+        ON DELETE SET NULL
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
+      
+insert into buy_book  (buy_id,book_id, amount) value
+(	1,	1,	1),
+(	1,	7,	2),
+(	2,	8,	2),
+(	3,	3,	2),
+(	3,	2,	1),
+(	3,	1,	1),
+(	4,	5,	1);
+
+CREATE TABLE step (
+    step_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_step varchar(30)
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
+      
+insert into step  (name_step) value
+("Оплата"),
+("Упаковка"),
+("Транспортировка"),
+("Доставка");
+
+CREATE TABLE buy_step  (
+    buy_step_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id INT,
+    step_id INT,
+    date_step_beg DATE,
+    date_step_end DATE,
+    FOREIGN KEY (buy_id)
+        REFERENCES buy (buy_id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (step_id)
+        REFERENCES step (step_id)
+        ON DELETE SET NULL
+)  ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
+      
+insert into buy_step   (buy_id,step_id, date_step_beg,date_step_end) value
+(	1, 	1,	"2020-02-20","2020-02-20"),
+(	1,	2,	"2020-02-20","2020-02-21"),
+(	1,	3,	"2020-02-22","2020-03-07"),
+(	1,	4,	"2020-03-08","2020-03-08"),
+(	2,	1,	"2020-02-28","2020-02-28"),
+(	2,	2,	"2020-02-29","2020-03-01"),
+(	2,	3,	"2020-03-02",		null),
+(	2,	4,			null,		null),
+(	3,	1,	"2020-03-05","2020-03-05"),
+(	3,	2,	"2020-03-05","2020-03-06"),
+(	3,	3,	"2020-03-06","2020-03-10"),
+(	3,	4,	"2020-03-11",		null),
+(	4,	1,	"2020-03-20	", 		null),
+(	4,	2,			null,		null),
+(	4,	3,	 	 	null,		null),
+(	4,	4,	 		null,		null);
+
+
