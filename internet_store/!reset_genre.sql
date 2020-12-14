7e03782f3c6b1bbd50ba19228bcda5404ea6b563
Aleksey <aleks-sh@mail.ru>
2020-12-14 06:49:25 +0300

    deleted:    internet_store/!reset_author.sql 	deleted:    internet_store/!reset_book!.sql 	deleted:    internet_store/!reset_genre.sql 	deleted:    internet_store/create schemas.sql

--- internet_store/!reset_genre.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `genre`;
-CREATE TABLE genre (
-      genre_id INT PRIMARY KEY AUTO_INCREMENT, 
-      name_genre VARCHAR(50) 
-      )ENGINE='InnoDB' AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-insert into genre (name_genre) value
-('Роман'),
-('Поэзия'),
-('Приключения');
--- internet_store/create schemas.sql
+++
@@ -1,1 +0,0 @@
-CREATE SCHEMA `internet_store` ;
